5d78736bacac85dd36b4f3fde363956a2b4c6701
Fyn! <113708207+Moodyxfyn@users.noreply.github.com>
2022-10-05 09:34:07 -0500

    Beta 1.1.0 Testing

---
+++ version 1.1 BETA TEST (1).py
@@ -0,0 +1,27 @@
+from multiprocessing.connection import answer_challenge
+
+
+player = input ('Whats your name?: ')
+
+print('hello,' + player)
+
+age = input('how old are you?: ')
+
+print ('welcome,'+player +age)
+
+playing = input ('is this correct?: ')
+
+if answer_challenge != "yes":
+    print ('alright, lets begin the simulation!')
+else:
+    quit()
+
+username = input ('choose a username! dont use personal information: ')
+
+playing = input('welcome,' +username)
+
+playing = input('Hey there! how are you, '+username)
+
+if playing != "good!" or "not great" or "bad.":
+    else:
+        print()
